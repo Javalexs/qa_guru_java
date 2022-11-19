50335509703967d88362f86d1f572020ecb6453b
Alexsey Fadeev <fadeevlesha@list.ru>
2022-11-20 00:50:01 +0300

    vichsleniya

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,24 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //пример переполнения
+        int a = 130;
+        byte b = a;
+        System.out.println(b);
+
+        //пример вычисления комбинаций типов данных
+        int intValue = 100;
+        byte byteValue = 5;
+        double doubleValue = 2.0;
+
+        System.out.println(intValue + doubleValue);//102.0
+        System.out.println(intValue / doubleValue);//50.0
+        System.out.println(intValue % doubleValue);//0.0
+
+        System.out.println(intValue + byteValue);//105
+        System.out.println(intValue / byteValue);//20
+        System.out.println(intValue % byteValue);//0
+
+
     }
 }
